febd2b3742c558316186236000c92e63471eda47
Mayank Mourya <mayank.mourya@dotsquares.com>
2023-04-21 16:47:32 +0530

    Deployment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel-Build Deployment-Change command sequence</title>
+        <title>Laravel-Build Deployment-Deployment</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
