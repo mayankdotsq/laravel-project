df16b1436bb1a06d1410f4dd42d7c94c7d5e5615
Mayank Mourya <mayank.mourya@dotsquares.com>
2023-04-21 16:03:28 +0530

    third Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel-second commit</title>
+        <title>Laravel-third commit</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
