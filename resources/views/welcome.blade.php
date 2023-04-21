3ea8fd322304ac5d1d1a8418b887b014f7741e3f
Mayank Mourya <mayank.mourya@dotsquares.com>
2023-04-21 17:49:52 +0530

    Laravel-Build Piple deployment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel-Build Deployment-Deployment</title>
+        <title>Laravel-Build Piple deployment</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
