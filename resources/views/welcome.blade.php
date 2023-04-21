6278e9273c093d510d8ff2198d61145f2d498a19
Mayank Mourya <mayank.mourya@dotsquares.com>
2023-04-21 18:31:58 +0530

    Build deployment using pipleline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel-Build deployment using pipleline2</title>
+        <title>Laravel-Build deployment using pipleline3</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
